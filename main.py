fe5ad2d66382af664fb7c7210312a1e0050e4ec3
luxzenuk 1337 <138058756+regainc@users.noreply.github.com>
2023-09-16 09:07:42 +0300

    main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import subprocess
 bright_purple = '\033[95;1m'
 reset = '\033[0m'
 green = '\033[92m'
-yellow = '\033[93m'
+yellow_color = '\033[93m'
 red = '\033[91m'
 bold = '\033[1m'
 
@@ -73,8 +73,9 @@ def whois_bot():
     global whois_results  # whois_results değişkenini global olarak kullanmak için
 
     while True:
-        domain_name = input(
-            "Domain name for whois: ")
+        # Kullanıcı girişini renklendirmek için renkli metni ekrana yazdırın
+        colored_domain_prompt = Colorate.Vertical(Colors.yellow_to_red, "Domain name for whois: ")
+        domain_name = input(f"{colored_domain_prompt}")
 
         if domain_name.lower() == "menu":
             whois_results = ""  # Sorgu sonuçlarını temizle
@@ -84,7 +85,7 @@ def whois_bot():
                 whois_info = whois.whois(domain_name)
 
                 if whois_info.status:
-                    whois_result = "Whois Bilgileri:\n"
+                    whois_result = f"Whois Bilgileri:\n"
                     whois_result += f"Alan Adı: {whois_info.domain_name}\n"
                     whois_result += f"Durum: {whois_info.status}\n"
                     whois_result += f"Oluşturulma Tarihi: {whois_info.creation_date}\n"
@@ -92,10 +93,9 @@ def whois_bot():
                     whois_result += f"Bitiş Tarihi: {whois_info.expiration_date}\n"
                     whois_result += f"Kayıt Sahibi: {whois_info.registrant_name}\n"
 
-                    # Sonuçları whois_results değişkenine ekleyin
-                    whois_results += whois_result + "\n"
-
-                    print(whois_result)
+                    # Kullanıcı girişi ve sorgu sonuçlarını aynı renkle yazdırın
+                    colored_result = Colorate.Vertical(Colors.yellow_to_red, whois_result)
+                    print(f"{colored_domain_prompt}{domain_name}\n{colored_result}")
                 else:
                     print(f"{domain_name} için whois bilgisi bulunamadı.")
             except whois.parser.WhoisException as e:
@@ -111,9 +111,8 @@ def whois_bot():
 
 while True:
     main_menu()
-
-    colored_choice = Colorate.Vertical(Colors.red_to_yellow, Center.XCenter(">>>>>>"))
-    choice = input(f"{colored_choice} ")
+    yellow_input = f"{yellow_color}>>>>>>{reset}"  # Sarı renkli giriş istemi
+    choice = input(yellow_input)
 
     if choice == 'whois':
         whois_bot()
